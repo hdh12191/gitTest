756a4711033e21a1f0ce837a3081e4e263e75008
hdh12191 <hdh12191@naver.com>
2023-10-23 19:00:51 +0900

    second commit

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-function sum(x, y) {
-  return x + y
-}
+import _ from 'lodash'
 
+const user = {
+  name: 'Heropy',
+  age: 85,
+  emails: ['thesecom@gamil.com']
+}
+const copyUser = _.cloneDeep(user)
+console.log(copyUser === user)
+
+user.age = 22
+console.log('user', user)
+console.log('copyUser', copyUser)
+
+console.log('------')
+console.log('------')
+
+user.emails.push('neo@zillnks.com')
+console.log(user.emails === copyUser.emails)
+console.log('user', user)
+console.log('copyUser', copyUser)
+// console.log('copyUser', copyUser)
